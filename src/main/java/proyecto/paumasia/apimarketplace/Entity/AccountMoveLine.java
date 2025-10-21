534b4dcd9c7415e64b21e4f598163e6d1103d70a
pmasia <dears2004pau@gmail.com>
2025-10-21 14:51:56 +0200

    21-20-25

--- src/main/java/proyecto/paumasia/apimarketplace/Entity/AccountMoveLine.java
+++ src/main/java/proyecto/paumasia/apimarketplace/Entity/AccountMoveLine.java
@@ -1,4 +1,21 @@
 package proyecto.paumasia.apimarketplace.Entity;
 
-public class AccountMoveLine {
+import proyecto.paumasia.apimarketplace.BaseModel;
+
+public class AccountMoveLine extends BaseModel {
+    public ProductProduct product_id;
+    public AccountMove move_id;
+    public Integer quantity;
+    public Integer line_discount; // %
+    public Integer product_price;
+    public Integer total_price;
+
+    public AccountMoveLine(ResPartner create_uid) {
+        super(create_uid);
+    }
+    // Se deberi de añadir tasas extra por linea, por si el envio tiene un coste extra en aduanas o algo
+    // public Integer extra_tax;
+    // realmente deberia de ser una lista de tasas y luego sacar el %, por si hay varias, no se va a tener el cuenta por ahora
+
+    public //metodo calculo precio lineas
 }
