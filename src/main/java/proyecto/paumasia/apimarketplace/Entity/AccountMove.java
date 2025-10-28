ac781323d4c5aab5b833a8034d4043cd30c86421
PauMasia <dears2004pau@gmail.com>
2025-10-28 19:36:23 +0100

    28-10-2025

--- src/main/java/proyecto/paumasia/apimarketplace/Entity/AccountMove.java
+++ src/main/java/proyecto/paumasia/apimarketplace/Entity/AccountMove.java
@@ -1,9 +1,36 @@
 package proyecto.paumasia.apimarketplace.Entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import proyecto.paumasia.apimarketplace.BaseModel;
 
+import java.util.List;
+
 public class AccountMove extends BaseModel {
-    public AccountMove(ResPartner create_uid) {
-        super(create_uid);
-    }
+
+    @Column(nullable = false)
+    private String name;
+    @JoinColumn(referencedColumnName = "id",nullable = false)
+    @OneToOne
+    private ResPartner partner_id;
+    @Column(nullable = false)
+    private String shipping_location;
+    private SaleOrder order_id; // No hace falta que sea nullable false porque se crearan apartir de las sale order, si no no
+    private List<AccountMoveLine> line_ids;
+    @Column(nullable = false)//total sin impuestos
+    private Float amount_untaxed;
+    @Column(nullable = false)//impuestoa
+    private Float amount_tax;
+    @Column(nullable = false) //Envio
+    private Float amount_delivery;
+    @Column(nullable = false)//Total con todo
+    private Float amount_total;
+//
+
+
+//    private String state; // Esta no hara falta, no es una mayorista payed, waiting
+//    public AccountMove(ResPartner create_uid) {
+//        super(create_uid);
+//    }
 }
